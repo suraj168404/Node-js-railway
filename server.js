2fe56240a0b6f019ca56bdb5fb2007b8f4c1a6d5
suraj168404 <rajmishra200302@tutamail.com>
2026-03-31 18:54:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 {
   "name": "raj-thakur-system",
   "version": "1.0.0",
-  "description": "Ultimate Advanced System for Facebook Automation",
   "main": "server.js",
   "scripts": {
     "start": "node server.js"
@@ -12,8 +11,5 @@
     "fca-mafiya": "latest",
     "uuid": "^9.0.0",
     "ws": "^8.14.2"
-  },
-  "engines": {
-    "node": "18.x"
   }
 }
